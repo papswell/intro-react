d591493e749a8c0ee6d31b1dbd8315a7b4fc0bc0
Pierre Guillaume <pierre@tricy.io>
2017-10-15 22:03:05 +0200

    Adding comments

--- src/app.jquery.js
+++ src/app.jquery.js
@@ -1,26 +1,71 @@
+/**
+ * This is a jQuery style for our application. It is imperative: we have
+ * to declare how the application works line by line.
+ * Even if the code here is not really well written, and could be refactored to
+ * remove duplication, we can see that we have to call the same pieces of code
+ * at multiple places. (updating counter, toggling alert box, ...). As the
+ * application grows and the requirements change, this can become problematic
+ * because we have to remember to update every parts of the code.
+ *
+ *
+ * NOTE the wrapper here ensures the DOM is fully loaded before executing any
+ * Javascript code.
+ * @see https://learn.jquery.com/using-jquery-core/document-ready/
+ */
 $(function() {
 
+  // NOTE We store jQuery elements inside variables so we don't have to reselect
+  // them any time we need them. By convention, to distinguish them froom
+  // "normal" variables we preffix the name with $.
   var $button = $('#jquery-app .tweet-button');
   var $textarea = $('#jquery-app textarea');
   var $counter  = $('#jquery-app .counter');
   var $photoButton = $('#jquery-app .photo-button');
   var $alert = $('#jquery-app .alert');
 
+  // internal state
   var isPhotoAdded = false;
 
+  /**
+   * NOTE As the code for the alert is needed in multiple places, we wrap it in
+   * a helper function.
+   *
+   * @param  {String} text     the text
+   * @param  {Number} maxValue the maximum length allowed
+   * @return {jQueryElt}          the html markup to put inside the alert box
+   */
   function getAlertContent(text, maxValue) {
-    return $('<span> ...' + text.slice(maxValue - 10, maxValue) + '<strong>' + text.slice(maxValue)+ '</strong></span>');
+    return $(
+      '<span>' +
+        ' ...' +
+        text.slice(maxValue - 10, maxValue) +
+        '<strong>' +
+          text.slice(maxValue) +
+        '</strong>' +
+      '</span>');
   }
 
   // Initial state, button is disabled by default, alert is hidden
   $button.prop('disabled', true);
   $alert.hide();
 
-  // Event listener, we update the button state if needed
+  // NOTE: Connect an EventListener to the textarea.
+  // Any time an event of type "input" happens on the textarea, the callback
+  // function is exectued. In this callback function, we can update the
+  // application state if needed.
   $textarea.on('input', function() {
+
+    // NOTE: We get the value of the textarea (ie the text that the user has typped)
+    // As the "input" event has been triggered on the textarea, the "this"
+    // special keyword refers to the textarea itself.
     const value = $(this).val();
     const valueLength = value.length;
 
+    /**
+     * Compute the max value and the remaining characters count.
+     * NOTE This is a ternary shortcut
+     * @see https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Op%C3%A9rateurs/L_op%C3%A9rateur_conditionnel
+     */
     const maxValue = isPhotoAdded ? 140 - 23 : 140;
     const remaining = maxValue - valueLength;
 
@@ -45,13 +90,19 @@ $(function() {
 
   });
 
+  // Connecting an EventListener on the photo button.
+  // Like above, the callback is executed any time the button is clicked.
   $photoButton.on('click', function() {
 
     // Toggle the variable
+    // NOTE: This is the simplest way to inverse a boolean value, a ternary equivalent
+    // would be:
+    // isPhotoAdded = isPhotoAdded ? false : true
     isPhotoAdded = !isPhotoAdded;
 
     if (isPhotoAdded) {
 
+      // Compute the max value and the remaining characters count.
       const maxValue = 140 - 23;
       const remaining = maxValue - $textarea.val().length;
 
@@ -73,6 +124,7 @@ $(function() {
           .find('span').html('');
       }
     } else {
+      // Compute the remaining characters count.
       const remaining = 140 - $textarea.val().length;
 
       // Set button content
--- src/app.react.js
+++ src/app.react.js
@@ -1,30 +1,75 @@
+/**
+ * This is the React part of our application.
+ * It is DECLARATIVE: we describe our User Interface (UI) in the render method of
+ * Components, connect some callbacks to update Components internal state,
+ * and everything is managed by React.
+ */
+
+/**
+ * TweetBox component. As the app is simple here, all the UI is described in this
+ * component.
+ * @type {React.Component}
+ */
 class TweetBox extends React.Component {
 
+  /**
+   * Constructor of the component.
+   * This function is called only once when the component is created.
+   */
   constructor() {
+    // We need to call the constructor of the parent class (React.Component)
     super();
 
+    // This component needs an internal state, which will evolve as the user
+    // uses the application. Any time the state is updated, the component re-render.
+    // We declare the initial state in the constructor.
     this.state = {
       text: '',
       isPhotoAdded: false,
     };
 
+    // Bind the internal methods of the component to itself, so that when these
+    // methods are called through event listener callbacks, the 'this' context
+    // is not lost.
     this.handleChange = this.handleChange.bind(this);
     this.handlePhotoClick = this.handlePhotoClick.bind(this);
   }
 
+  /**
+   * Callback function, triggered any tme the user type something in the
+   * textarea. React automatically binds a SyntheticEvent as first parameter to
+   * change callbacks, that can be used according our needs.
+   * @see https://reactjs.org/docs/events.html
+   *
+   * @param  {SyntheticEvent} event
+   */
   handleChange(event) {
     const newTextValue = event.target.value;
+
+    // Update the state with the new textarea value.
+    // NOTE we have to call setState() method to update the state and ensures
+    // that the component re-renders correctly
+    // !!! Doing this.state.text = "NEW TEXT" will not work !!!
     this.setState({
       text: newTextValue,
     })
   }
 
+  /**
+   * Callback triggered whenthe user clicks on the photo button
+   * @param  {[SyntheticEvent]} event the click event
+   */
   handlePhotoClick(event) {
     this.setState({
       isPhotoAdded: !this.state.isPhotoAdded,
     });
   }
 
+  /**
+   * Helper method computing the number of characters that the user can still
+   * write according to the state of the component
+   * @return {Number}
+   */
   getRemainingCharacters() {
     let remaining = 140 - this.state.text.length;
     if (this.state.isPhotoAdded) {
@@ -33,6 +78,11 @@ class TweetBox extends React.Component {
     return remaining;
   }
 
+  /**
+   * Helper method that render the alert box according to the state of the
+   * component
+   * @return {JSX} the JSX markup for the alert box
+   */
   renderOverflowAlert() {
     const { text, isPhotoAdded } = this.state;
     const maxValue = isPhotoAdded ? 140 - 23 : 140;
@@ -54,6 +104,13 @@ class TweetBox extends React.Component {
     )
   }
 
+  /**
+   * This method is called once when the component mounts, and then any time the
+   * state of the component is updated via setState(...)
+   * It describe all the UI, and connects the change callbacks to it.
+   *
+   * @return {JSX} the JSX markup of the TweetBox UI.
+   */
   render() {
     const { text, isPhotoAdded } = this.state;
 
@@ -86,6 +143,9 @@ class TweetBox extends React.Component {
   }
 };
 
+/**
+ * Mount a TweetBox component in the DOM.
+ */
 ReactDOM.render(
   <TweetBox />,
   document.getElementById("react-app")
